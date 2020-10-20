483cbb18fa4ca8b3f9aef9d7c1548f0b0b9e2aa0
Cody Stubbs <69646002+codyastubbs@users.noreply.github.com>
2020-10-19 21:53:49 -0400

    Chord visualizer function now visualizes only keys for the middle of an 88-key piano, specifically showing the chord's keys for the seventh octave.

--- scripts/piano_visualizer.R
+++ scripts/piano_visualizer.R
@@ -6,7 +6,7 @@ k_data <-
         mutate(viz.y = gsub("white", "1", key.color)) %>%
         mutate(viz.y = gsub("black", "0.667", viz.y)) %>% 
         mutate(viz.y = as.numeric(viz.y)) %>% 
-        separate_rows(notes, sep = " ")
+        separate_rows(notes, sep = " ") 
 
 p <- k_data %>% 
         ggplot(aes(x = viz.x, y = viz.y, fill = key.color)) +
@@ -62,9 +62,9 @@ pb$layers <- c(pb$layers[1], pb$layers[5], pb$layers[2], pb$layers[3], pb$layers
 pb
 
 
-notes_lst <- c("A", "B", "C#")
-
-display_notes <- function(chord = NULL, notes_lst){
+notes_lst <- c("A", "C#", "E")
+chrd <- "A major"
+display_notes <- function(chrd = NULL, notes_lst){
         # Load data for piano ----
         k_data <- 
                 read_csv("data/key-data-88 - keys.csv") %>% 
@@ -72,9 +72,11 @@ display_notes <- function(chord = NULL, notes_lst){
                 mutate(viz.y = gsub("white", "1", key.color)) %>%
                 mutate(viz.y = gsub("black", "0.667", viz.y)) %>% 
                 mutate(viz.y = as.numeric(viz.y))  %>% 
-                separate_rows(notes, sep = " ")
+                separate_rows(notes, sep = " ") %>% 
+                filter(key.number >= 28, key.number <= 63) 
         
         p <- k_data %>% 
+                filter(octave == 4) %>% 
                 ggplot(aes(x = viz.x, y = viz.y, fill = key.color)) +
                 geom_col(
                         data = k_data %>% filter(key.color == "white"),
@@ -85,24 +87,6 @@ display_notes <- function(chord = NULL, notes_lst){
                         data = k_data %>% filter(key.color == "black"),
                         position = position_dodge(), color = "white",
                         width = 0.75
-                ) +
-                geom_text(
-                        data = k_data %>% filter(key.color == "white"),
-                        color = "black",
-                        aes(label = notes.1),
-                        angle = 90, hjust = "left"
-                ) +
-                geom_text(
-                        data = k_data %>% filter(key.color == "white"),
-                        color = "black",
-                        aes(label = notes.2),
-                        angle = 90, hjust = "left"
-                ) +
-                geom_text(
-                        data = k_data %>% filter(key.color == "black"),
-                        color = "white",
-                        aes(label = notes),
-                        angle = 90, hjust = "left"
                 ) +
                 scale_fill_manual(values = c("black", "white")) +
                 scale_y_reverse(
@@ -124,26 +108,93 @@ display_notes <- function(chord = NULL, notes_lst){
                       legend.position = "none")
         
         #----
-        if (chord = NULL){
+        if (chrd = NULL){
                 visual_data <- k_data %>% 
+                        filter(octave == 4) %>% 
                         filter(notes %in% notes_lst)
                 
-                p_w_chord <- 
-                        p + geom_col( 
-                                data = visual_data,
+                p_w_chord <- # The piano build ####
+                        p + 
+                        geom_text(
+                                data = k_data %>% filter(key.color == "white"),
+                                color = "black",
+                                aes(label = notes),
+                                angle = 0, nudge_y = 0.1
+                        ) +
+                        geom_text(
+                                data = k_data %>% filter(key.color == "black", str_detect(notes, "#$")),
+                                color = "white",
+                                aes(label = notes),
+                                angle = 0, nudge_y = 0.125
+                        ) +
+                        geom_text(
+                                data = k_data %>% filter(key.color == "black", str_detect(notes, "b$")),
+                                color = "white",
+                                aes(label = notes),
+                                angle = 0, nudge_y = 0.075
+                        ) +
+                        geom_col( 
+                                data = visual_data %>% filter(key.color == "black"),
                                 aes(x = viz.x, y = viz.y),
-                                fill = "gold", color = "black",
+                                fill = "purple", color = "black",
+                                width = 0.75 ) + 
+                        geom_col( 
+                                data = visual_data %>% filter(key.color == "white"),
+                                aes(x = viz.x, y = viz.y),
+                                fill = "purple", color = "black",
                                 width = 1 )
+                # Rearrange the plot layers to optimize visibility ####
+                p_w_chord$layers <- c(p_w_chord$layers[1], p_w_chord$layers[7], p_w_chord$layers[2], p_w_chord$layers[6], p_w_chord$layers[3], p_w_chord$layers[4], p_w_chord$layers[5])
                 
-                p_w_chord$layers <- c(p_w_chord$layers[1], p_w_chord$layers[5], p_w_chord$layers[2], p_w_chord$layers[3], p_w_chord$layers[4])
+                # Print the figure
                 p_w_chord
+                
         } else{
                 chord_data <- read_csv("data/chords_01_cleaned.csv") %>% 
-                        filter(key_chord_short == chord) %>% 
-                        select(key_chord_short, notes)
+                        filter(key_chord_short %in% chrd) %>% 
+                        select(key_chord_short, notes) %>% 
+                        filter(notes %in% chord_data$notes)
                 
                 visual_data <- k_data %>% 
-                        filter(notes %in% notes_lst)
+                        filter(octave == 4) %>% 
+                        filter(notes %in% chord_data$notes)
+                
+                p_w_chord <- # The piano build ####
+                p + 
+                        geom_text(
+                                data = k_data %>% filter(key.color == "white"),
+                                color = "black",
+                                aes(label = notes),
+                                angle = 0, nudge_y = 0.1
+                        ) +
+                        geom_text(
+                                data = k_data %>% filter(key.color == "black", str_detect(notes, "#$")),
+                                color = "white",
+                                aes(label = notes),
+                                angle = 0, nudge_y = 0.125
+                        ) +
+                        geom_text(
+                                data = k_data %>% filter(key.color == "black", str_detect(notes, "b$")),
+                                color = "white",
+                                aes(label = notes),
+                                angle = 0, nudge_y = 0.075
+                        ) +
+                        geom_col( 
+                                data = visual_data %>% filter(key.color == "black"),
+                                aes(x = viz.x, y = viz.y),
+                                fill = "purple", color = "black",
+                                width = 0.75 ) + 
+                        geom_col( 
+                                data = visual_data %>% filter(key.color == "white"),
+                                aes(x = viz.x, y = viz.y),
+                                fill = "purple", color = "black",
+                                width = 1 )
+                
+                # Rearrange the plot layers to optimize visibility ####
+                p_w_chord$layers <- c(p_w_chord$layers[1], p_w_chord$layers[7], p_w_chord$layers[2], p_w_chord$layers[6], p_w_chord$layers[3], p_w_chord$layers[4], p_w_chord$layers[5])
+                
+                # Print the figure
+                p_w_chord
                 
                 
         }
